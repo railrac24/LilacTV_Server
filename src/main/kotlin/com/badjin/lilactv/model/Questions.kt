f506e4e660c5b5c1fa743355825a932d6426d68d
badjin <railrac23@gmail.com>
2019-08-03 12:57:46 +1000

    LilactTV update

--- src/main/kotlin/com/badjin/lilactv/model/Questions.kt
+++ src/main/kotlin/com/badjin/lilactv/model/Questions.kt
@@ -1,5 +1,6 @@
 package com.badjin.lilactv.model
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import javax.persistence.*
@@ -8,13 +9,18 @@ import javax.persistence.*
 data class Questions (
         @ManyToOne(cascade = [CascadeType.MERGE])
         @JoinColumn(name = "writer_id")
+        @JsonProperty
         var writer: Users,
+        @JsonProperty
         var title: String,
+        @JsonProperty
         var content: String,
         var createDate: LocalDateTime,
         @OneToMany(mappedBy = "questions", cascade = [CascadeType.ALL])
-        @OrderBy("id ASC")
+        @OrderBy("id DESC")
+        @JsonProperty
         var answers: MutableList<Answers>?,
+        @JsonProperty
         @Id @GeneratedValue var id: Long? = null
 ) {
     constructor(writer: Users, title: String,content: String): this(writer, title, content, LocalDateTime.now(), null)
