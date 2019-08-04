674e35a3f89b59baaafb3d6d01316c5ec98f5d5c
badjin <railrac23@gmail.com>
2019-08-05 07:00:14 +1000

    LilactTV update

--- src/main/kotlin/com/badjin/lilactv/controller/BookController.kt
+++ src/main/kotlin/com/badjin/lilactv/controller/BookController.kt
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -19,25 +20,29 @@ class BookController {
     @Autowired
     lateinit var serviceModule: LilacTVServices
 
-    @GetMapping("/listBooks")
+    @GetMapping("/listBooks/{page}/{size}")
     fun listBooks(
             model: Model,
-            @RequestParam("page") page: Optional<Int>,
-            @RequestParam("size") size: Optional<Int>): String {
+            @PathVariable page: Optional<Int>,
+            @PathVariable size: Optional<Int>): String {
         val currentPage = page.orElse(1)
         val pageSize = size.orElse(5)
         val units = serviceModule.findPaginated(PageRequest.of(currentPage - 1, pageSize))
-
+        val actives = Array(units.totalPages) {false}
+        actives[currentPage-1] = true
         model["units"] = units
 
         val totalPages = units.totalPages
         if (totalPages > 0) {
-            val pageNumbers = Array(totalPages) { i -> (i+1).toString() }
-//            val pageNumbers = IntStream.rangeClosed(1, totalPages).boxed()
-//                    .collect<List<Int>, Any>(Collectors.toList())
+            var pageNumbers = mapOf<String, String>()
+            for (i in totalPages)
+//            val pageNumbers = Array(totalPages) { i -> (i+1).toString() }
+
+            actives[currentPage-1] = true
             model["pageNumbers"] = pageNumbers
+            model["active"] = actives
+
             if (currentPage == 1) model["firstPage"] = true
-            if (currentPage == units.number+1) model["active"] = true
             if (currentPage == totalPages) model["lastPage"] = true
 
             for (i in pageNumbers) print("i = $i,")
